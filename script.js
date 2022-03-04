3efa37a1d62ef3803afe9188f00b6e1445617afa
Irene Njeru <irenenjeru8@gmail.com>
2022-03-04 18:12:03 +0300

    added the dayOfTheWeek variable

--- script.js
+++ script.js
@@ -30,11 +30,42 @@ function akannames(event) {
 
   if (
     (day == null || day == "",
-      month == null || month == "",
-      year == null || year == "")
+    month == null || month == "",
+    year == null || year == "")
   ) {
     alert("Please Fill All Required Field");
     return false;
   }
+  if (day <= 0 || day > 31) {
+    alert("enter valid date");
+    return false;
+  } else if (month <= 0 || month > 12) {
+    alert("enter valid month");
+    return false;
+  }
 
+  var sliceY = year % 100;
+  var sliceC = Math.floor(year / 100);
+  var c = parseInt(sliceC);
+  var y = parseInt(sliceY);
+  var d = day;
+  var m = month;
 
+  var dayOfTheWeek = Math.round(
+    (c / 4 - 2 * c - 1 + (5 * y) / 4 + (26 * (m + 1)) / 10 + d) % 7
+  );
+
+  if (male.checked == true) {
+    for (var a = 0; a < maleName.length; a++) {
+      if (a === dayOfTheWeek - 1) {
+        rname.innerHTML = "Your Akan name is " + maleName[a];
+      }
+    }
+  } else if (female.checked == true) {
+    for (var a = 0; a < femaleName.length; a++) {
+      if (a === dayOfTheWeek - 1) {
+        rname.innerHTML = "Your Akan name is " + femaleName[a];
+      }
+    }
+  }
+}
