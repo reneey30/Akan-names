c85ab48e82fe42f273f45319db70f6afc4d58ca5
Irene Njeru <irenenjeru8@gmail.com>
2022-03-04 14:31:12 +0300

    removed the normal brackets on the scriot.js file and but the square brackets

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var CC, DD, MM, YY, d, dayvalue;
-var weekDays=("Sunday","Monday","Tueday","Wednesday","Thursday","Friday","Saturday");
-var femaleNames=("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama");
-var maleNames=("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame");
+var weekDays=["Sunday","Monday","Tueday","Wednesday","Thursday","Friday","Saturday"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
 
 
 
